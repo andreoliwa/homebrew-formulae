25e40dc5377c8eb0883684318a12d3f305ab1301
W. Augusto Andreoli <andreoliwa@gmail.com>
2022-01-16 05:35:07 +0100

    build: latest release 0.31.0
    - invoke brew-python
    - build: use URL and SHA from PyPI
    - ci: skip local pre-commit hook
    - docs: update README and task help
    - docs: steps to update the version
    
    Closes #5.
    
    Signed-off-by: W. Augusto Andreoli <andreoliwa@gmail.com>

--- tasks.py
+++ tasks.py
@@ -3,8 +3,9 @@
 - https://github.com/pyinvoke/invoke
 - http://docs.pyinvoke.org/en/stable/api/runners.html#invoke.runners.Runner.run
 """
-from invoke import task, Exit
 from pathlib import Path
+
+from invoke import Exit, task
 
 # Package config by formula
 PACKAGES_BY_FORMULA = {
@@ -22,7 +23,7 @@ PACKAGES_BY_FORMULA = {
 def choose_formula(c) -> str:
     """Choose a formula from the formulae."""
     rv = c.run(
-        "fd .rb Formula/ | rg -o 'Formula/(.+)\.rb' -r '$1' | fzf --reverse --height 40% --select-1"
+        r"fd .rb Formula/ | rg -o 'Formula/(.+)\.rb' -r '$1' | fzf --reverse --height 40% --select-1"
     ).stdout.strip()
     if not rv:
         raise Exit()
@@ -36,9 +37,16 @@ def formula_name_with_tap(formula: str) -> str:
     return f"{user}/{repo}/{formula}"
 
 
-@task
+@task(
+    help={
+        "interactive": "Install the package and open an interactive shell to test it",
+        "head": "Install the HEAD version",
+        "debug": "Debug logs",
+        "test_only": "Test only",
+    }
+)
 def brew_install(c, interactive=False, head=False, debug=False, test_only=False):
-    """Install a Homebrew formula."""
+    """Install a Homebrew formula. Detect formula and user name from the current dirs."""
     formula = choose_formula(c)
     full_formula_name = formula_name_with_tap(formula)
 
